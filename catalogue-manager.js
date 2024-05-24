dc8185e3f13e1eb4d513dc865b447bcd062080ec
EnvyTia <helmiabiyu20@gmail.com>
2024-05-24 10:03:01 +0700

    some update on dashboard

--- catalogue-manager.js
+++ catalogue-manager.js
@@ -1,14 +1,19 @@
-document.querySelectorAll('.container-text h4').forEach(header => {
-  header.addEventListener('click', () => {
-      const targetId = header.getAttribute('data-target');
-      const targetElement = document.getElementById(targetId);
+let slideIndex = 0;
+showSlides();
 
-      document.querySelectorAll('.dropdown-explanation').forEach(explanation => {
-          if (explanation.id === targetId) {
-              explanation.classList.add('expanded');
-          } else {
-              explanation.classList.remove('expanded');
-          }
-      });
-  });
-});
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("slider");
+  let dots = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
