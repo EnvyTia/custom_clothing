ddafd4ead770cee370fcfed1c2a6a51a1db884e4
EnvyTia <helmiabiyu20@gmail.com>
2024-05-25 10:19:07 +0700

    some update on catalogue

--- JS/catalogue-manager.js
+++ JS/catalogue-manager.js
@@ -1,19 +1,41 @@
+// function toggleDropdown(index) {
+//   // Hide all dropdowns
+//   var dropdowns = document.querySelectorAll('.dropdown-content');
+//   dropdowns.forEach(function(dropdown) {
+//       dropdown.classList.remove('show');
+//   });
+
+//   // Show the selected dropdown
+//   var selectedDropdown = document.getElementById('dropdown' + index);
+//   selectedDropdown.classList.add('show');
+  
+//   // Highlight the corresponding slide
+//   var slides = document.querySelectorAll('.slider img');
+//   slides.forEach(function(slide) {
+//       slide.classList.remove('active');
+//   });
+//   var selectedSlide = document.getElementById('slide' + index);
+//   selectedSlide.classList.add('active');
+// }
 function toggleDropdown(index) {
-  // Hide all dropdowns
-  var dropdowns = document.querySelectorAll('.dropdown-content');
+  // Find the parent section of the clicked button
+  var section = document.querySelector('#dropdown' + index).closest('.catalogues');
+  
+  // Hide all dropdowns within this section
+  var dropdowns = section.querySelectorAll('.dropdown-content');
   dropdowns.forEach(function(dropdown) {
       dropdown.classList.remove('show');
   });
 
-  // Show the selected dropdown
-  var selectedDropdown = document.getElementById('dropdown' + index);
+  // Show the selected dropdown within this section
+  var selectedDropdown = section.querySelector('#dropdown' + index);
   selectedDropdown.classList.add('show');
   
-  // Highlight the corresponding slide
-  var slides = document.querySelectorAll('.slider img');
+  // Highlight the corresponding slide within this section
+  var slides = section.querySelectorAll('.slider img');
   slides.forEach(function(slide) {
       slide.classList.remove('active');
   });
-  var selectedSlide = document.getElementById('slide' + index);
+  var selectedSlide = section.querySelector('#slide' + index);
   selectedSlide.classList.add('active');
 }
